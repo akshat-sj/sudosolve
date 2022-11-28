823274821502df6aea5ca684b22343e114250c7a
akshat-sj <sja.akshat@gmail.com>
2022-11-28 20:14:32 +0530

    validity of sudoku matrix code
    Basically checks if our guesses are valid or not in matrix

--- main.py
+++ main.py
@@ -4,3 +4,25 @@ def empty_finder(sudoku):
             if sudoku[r][c] == -1:
                 return r,c
 
+    return None,None
+
+def validity(sudoku,sol,r,c):
+    rval = sudoku[r]
+    if sol in rval:
+        return False
+    cval =[]
+    for i in range(9):
+        cval.append(sudoku[i][c])
+    if sol in cval:
+        return False
+
+    r_i=(r//3)*3
+    c_i=(c//3)*3
+    for i in range(r_i,r_i +3):
+        for j in range(c_i,c_i+3):
+            if sudoku[i][j]==sol:
+                return False
+
+    return True
+
+
